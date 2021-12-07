ffa9fdc79eebe16aa19b68c182dbf0838ba91b1c
Victor Ojewole <70405911+vojewole@users.noreply.github.com>
2021-12-07 14:48:26 -0800

    Delete ProductManager directory

--- ProductManager/server/routes/productManager.routes.js
+++
@@ -1,7 +0,0 @@
-const ProductController = require('../controllers/productManager.controller')
-
-module.exports = app => {
-    app.get('/api/product', ProductController.findAllProducts);
-    app.post('/api/product', ProductController.createProduct);
-}
-
--- ProductManager/server/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const cors = require('cors'); 
-const app = express();
-require('./config/productManager.config');
-app.use(cors());  
-app.use(express.json(), express.urlencoded({extended: true}));
-const port = 3000;
-require('../server/routes/productManager.routes')(app);
-app.listen(port, console.log('Listening on port 3000.'))
