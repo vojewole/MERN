40584ff45bd193cfd3894e84188c340ee883c9c5
Victor Ojewole <70405911+vojewole@users.noreply.github.com>
2021-12-08 14:20:53 -0800

    Delete ProductManager directory

--- ProductManager/server/routes/productManager.routes.js
+++
@@ -1,8 +0,0 @@
-const ProductController = require('../controllers/productManager.controller')
-
-module.exports = app => {
-    app.get('/api/product', ProductController.findAllProducts);
-    app.get('/api/product/:id', ProductController.getProduct);
-    app.post('/api/product', ProductController.createProduct);
-}
-
