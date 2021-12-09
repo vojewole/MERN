3ba3a1718c806e8892090be2444324ed37985cc3
Victor Ojewole <70405911+vojewole@users.noreply.github.com>
2021-12-08 17:41:57 -0800

    Delete ProductManager directory

--- ProductManager/server/config/productManager.config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/productmanagerdb', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log("Established a connection to the database."))
-.catch(err => console.log("Soemthing went wrong when connecting to the database", err));
-
