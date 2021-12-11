09492d61f2b92748229f8829d6370da4f2851625
Victor Ojewole <70405911+vojewole@users.noreply.github.com>
2021-12-10 16:32:39 -0800

    Delete Author directory

--- Author/server/config/author.config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/authordb', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log("Established a connection to the database."))
-.catch(err => console.log("Soemthing went wrong when connecting to the database", err));
-
